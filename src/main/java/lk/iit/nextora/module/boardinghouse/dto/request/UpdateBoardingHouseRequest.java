45d651c9bb5b927a750150c4f47dfdc1079e5276
isuru38 <isururavishan0627@gmail.com>
2026-02-19 08:15:31 +0530

    Task-1012.12: add UpdateBoardingHouseRequest DTO for boarding house update requests

---
+++ src/main/java/lk/iit/nextora/module/boardinghouse/dto/request/UpdateBoardingHouseRequest.java
@@ -0,0 +1,32 @@
+package lk.iit.nextora.module.boardinghouse.dto.request;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateBoardingHouseRequest {
+
+    private String name;
+    private String description;
+    private Double monthlyRent;
+    private Boolean keyMoneyRequired;
+    private String genderType;
+    private Boolean withFood;
+    private Boolean withFurniture;
+
+    // Location
+    private String city;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+
+    // Contact
+    private String contactNumber1;
+    private String contactNumber2;
+
+    // Bills
+    private Boolean waterBillIncluded;
+    private Boolean electricityBillIncluded;
+}
